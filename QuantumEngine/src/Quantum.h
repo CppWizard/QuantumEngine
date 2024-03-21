75873a9cfab2a36d38edc39bbd43a64e79e4ab83
Greg Toney <gmtoney@outlook.com>
2024-03-21 16:52:01 -0400

    uploaded updated files

--- QuantumEngine/src/Quantum.h
+++ QuantumEngine/src/Quantum.h
@@ -3,6 +3,7 @@
 // For use by Hazel applications
 
 #include "QuantumEngine/Application.h"
+#include "QuantumEngine/Log.h"
 
 // ---Entry Point---------------------
 #include "QuantumEngine/EntryPoint.h"
--- QuantumEngine/src/QuantumEngine/EntryPoint.h
+++ QuantumEngine/src/QuantumEngine/EntryPoint.h
@@ -6,6 +6,11 @@ extern Quantum::Application* Quantum::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Quantum::Log::Init();
+	QT_CORE_WARN("Initialized Log!");
+	int a = 5;
+	QT_INFO("Hello!Var = {0}", a);
+
 	auto app = Quantum::CreateApplication();
 	app->Run();
 	delete app;
