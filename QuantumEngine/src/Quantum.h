cb5610a30751d9acf111db04c21483db753110d3
Greg Toney <gmtoney@outlook.com>
2024-03-21 16:05:47 -0400

    uploaded QuantumEngine project

---
+++ QuantumEngine/src/Quantum.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Hazel applications
+
+#include "QuantumEngine/Application.h"
+
+// ---Entry Point---------------------
+#include "QuantumEngine/EntryPoint.h"
+// -----------------------------------
---
+++ QuantumEngine/src/QuantumEngine/EntryPoint.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef QT_PLATFORM_WINDOWS
+
+extern Quantum::Application* Quantum::CreateApplication();
+
+int main(int argc, char** argv)
+{
+	auto app = Quantum::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+#endif
