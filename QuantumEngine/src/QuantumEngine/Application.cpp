592a82cd9ca06fa8b7e7f35f959eaa77eedf1ff9
Greg Toney <gmtoney@outlook.com>
2024-03-22 15:53:16 -0400

    uploaded events files

--- QuantumEngine/src/QuantumEngine/Application.cpp
+++ QuantumEngine/src/QuantumEngine/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "QuantumEngine/Events/AppEvent.h"
+#include "QuantumEngine/Log.h"
+
 namespace Quantum {
 
 	Application::Application()
@@ -13,6 +16,16 @@ namespace Quantum {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			QT_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			QT_TRACE(e);
+		}
+
 		while (true);
 	}
 
---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "QuantumEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+disablewarnings "4996"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "QuantumEngine"
+	location "QuantumEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"QT_PLATFORM_WINDOWS",
+			"QT_BUILD_DLL"
+		}
+
+	postbuildcommands
+	{
+		("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+	}
+
+	filter "configurations:Debug"
+		defines "QT_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "QT_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "QT_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"QuantumEngine/vendor/spdlog/include",
+		"QuantumEngine/src"
+	}
+
+	links
+	{
+		"QuantumEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"QT_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "QT_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "QT_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "QT_DIST"
+		optimize "On"
