45a4d47465a389611e3607ba2e05fe3b355ba7a6
Greg Toney <gmtoney@outlook.com>
2024-03-23 20:35:32 -0400

    added updated event files for window events

--- QuantumEngine/src/QuantumEngine/Application.cpp
+++ QuantumEngine/src/QuantumEngine/Application.cpp
@@ -1,16 +1,26 @@
 #include "qtpch.h"
 #include "Application.h"
 
-#include "QuantumEngine/Events/AppEvent.h"
 #include "QuantumEngine/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Quantum {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		QT_CORE_TRACE("{0}", e);
 	}
 
 
@@ -26,6 +36,12 @@ namespace Quantum {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
