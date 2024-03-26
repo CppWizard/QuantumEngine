cbc09ffd7c0a38f292ec3482af88969f4a4da0bb
Greg Toney <gmtoney@outlook.com>
2024-03-26 13:47:55 -0400

    added OpenGL context class

--- QuantumEngine/src/Platform/Windows/WindowsWindow.cpp
+++ QuantumEngine/src/Platform/Windows/WindowsWindow.cpp
@@ -5,7 +5,7 @@
 #include "QuantumEngine/Events/MouseEvent.h"
 #include "QuantumEngine/Events/KeyEvent.h"
 
-#include <glad/glad.h>
+#include "Platform/OpenGL/OpenGLContext.h"
 
 namespace Quantum {
 
@@ -50,9 +50,10 @@ namespace Quantum {
 		}
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
-		glfwMakeContextCurrent(m_Window);
-		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-		QT_CORE_ASSERT(status, "Failed to initialize Glad!");
+		
+		m_Context = new OpenGLContext(m_Window);
+		m_Context->Init();
+
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
 
@@ -156,7 +157,7 @@ namespace Quantum {
 	void WindowsWindow::OnUpdate()
 	{
 		glfwPollEvents();
-		glfwSwapBuffers(m_Window);
+		m_Context->SwapBuffers();
 	}
 
 	void WindowsWindow::SetVSync(bool enabled)
--- QuantumEngine/src/Platform/Windows/WindowsWindow.h
+++ QuantumEngine/src/Platform/Windows/WindowsWindow.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "QuantumEngine/Window.h"
+#include "QuantumEngine/Renderer/GraphicsContext.h"
 
 #include <GLFW/glfw3.h>
 
@@ -28,6 +29,7 @@ namespace Quantum {
 		virtual void Shutdown();
 	private:
 		GLFWwindow* m_Window;
+		GraphicsContext* m_Context;
 
 		struct WindowData
 		{
