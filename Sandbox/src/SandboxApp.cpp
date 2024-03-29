650490ea801ed731872381d53654bf1254e6ecdf
Greg Toney <gmtoney@outlook.com>
2024-03-28 21:26:01 -0400

    Moved OpenGL implementation to OpenGLShader class

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,11 @@
 #include <Quantum.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer : public Quantum::Layer
 {
@@ -85,9 +88,9 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new Quantum::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Quantum::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -104,18 +107,22 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
+
 			in vec3 v_Position;
+
+			uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new Quantum::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(Quantum::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(Quantum::Timestep ts)
@@ -145,13 +152,16 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<Quantum::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<Quantum::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Quantum::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				Quantum::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -162,7 +172,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-		
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(Quantum::Event& event) override
@@ -174,7 +186,7 @@ public:
 		std::shared_ptr<Quantum::Shader> m_Shader;
 		std::shared_ptr<Quantum::VertexArray> m_VertexArray;
 
-		std::shared_ptr<Quantum::Shader> m_BlueShader;
+		std::shared_ptr<Quantum::Shader> m_FlatColorShader;
 		std::shared_ptr<Quantum::VertexArray> m_SquareVA;
 
 		Quantum::OrthoGraphicCamera m_Camera;
@@ -183,6 +195,8 @@ public:
 
 		float m_CameraRotation = 0.0f;
 		float m_CameraRotationSpeed = 180.0f;
+
+		glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class Sandbox : public Quantum::Application
