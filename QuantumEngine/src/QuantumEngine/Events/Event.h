7c297caf654f06533fd7a7623d92024a8d2a3a30
Greg Toney <gmtoney@outlook.com>
2024-03-29 22:22:26 -0400

    shaders are now read from files

--- QuantumEngine/src/QuantumEngine/Events/Event.h
+++ QuantumEngine/src/QuantumEngine/Events/Event.h
@@ -51,8 +51,6 @@ namespace Quantum {
 
 	class EventDispatcher
 	{
-		template<typename T>
-		using EventFn = std::function<bool(T&)>;
 
 	public:
 		EventDispatcher(Event& event)
@@ -60,12 +58,13 @@ namespace Quantum {
 		{
 		}
 
-		template<typename T>
-		bool Dispatch(EventFn<T> func)
+		// F will be deduced by the compiler
+		template<typename T, typename F>
+		bool Dispatch(const F& func)
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.Handled = func(*(T*)&m_Event);
+				m_Event.Handled = func(static_cast<T&>(m_Event));
 				return true;
 			}
 			return false;
