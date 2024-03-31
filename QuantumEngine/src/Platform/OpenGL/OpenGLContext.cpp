30c667497d0c7a941a0045a0153067f47f899bae
Greg Toney <gmtoney@outlook.com>
2024-03-30 21:36:19 -0400

    updates

--- QuantumEngine/src/Platform/OpenGL/OpenGLContext.cpp
+++ QuantumEngine/src/Platform/OpenGL/OpenGLContext.cpp
@@ -26,6 +26,15 @@ namespace Quantum {
 		QT_CORE_INFO("  Vendor: {0}", (char*)glGetString(GL_VENDOR));
 		QT_CORE_INFO("  Renderer: {0}", (char*)glGetString(GL_RENDERER));
 		QT_CORE_INFO("  Version: {0}", (char*)glGetString(GL_VERSION));
+
+#ifdef HZ_ENABLE_ASSERTS
+		int versionMajor;
+		int versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+		HZ_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "QuantumEngine requires at least OpenGL version 4.5!");
+#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
