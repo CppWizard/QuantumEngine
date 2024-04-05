50b1c5650f6feb5db1c91ef991534dcc34354c62
Greg Toney <gmtoney@outlook.com>
2024-04-04 21:17:34 -0400

    Updated Sandbox2D

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -38,9 +38,9 @@ void Sandbox2D::OnUpdate(Quantum::Timestep ts)
 	{
 		QT_PROFILE_SCOPE("Renderer Draw");
 		Quantum::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		Quantum::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Quantum::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
 		Quantum::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		Quantum::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
+		Quantum::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
 		Quantum::Renderer2D::EndScene();
 	}
 }
